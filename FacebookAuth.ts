b85cf8a9bab53094adf7132db71f9848bc524d6e
Tarek <tarek22042002triki@gmail.com>
2023-09-23 13:22:57 +0100

    Update FacebookAuth.ts

--- FacebookAuth.ts
+++ FacebookAuth.ts
@@ -1,69 +1,69 @@
 export class FBAuth {
-    appId: Number;
-    accesToken:any;
+  appId: Number;
+  accesToken:any;
 
-    constructor(appId:Number) {
-        this.appId = appId;
-        const scriptTag = document.createElement("script");
-      // Set the JavaScript code as the content of the script
-      scriptTag.innerText  = `
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId: '${this.appId}',
-          autoLogAppEvents: true,
-          xfbml: true,
-          version: 'v12.0' // Use the desired version of the API
-        });
-      };
-    
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    
-      `;
+  constructor(appId:Number) {
+      this.appId = appId;
+      const scriptTag = document.createElement("script");
+    // Set the JavaScript code as the content of the script
+    scriptTag.innerText  = `
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: '${this.appId}',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v12.0' // Use the desired version of the API
+      });
+    };
+  
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  
+    `;
 
-      // Append the script element to the document's <head> or <body>
-      document.body.append(scriptTag);
-    }
+    // Append the script element to the document's <head> or <body>
+    document.head.appendChild(scriptTag);
+  }
 
-    login = async () => {
-       let FB:any = window;
-       try {
-        await FB.FB.login((response:any) => {
-          if (response.authResponse) {            
-            this.accesToken = response.authResponse.accessToken;            
-          } else {
-            console.log("User cancelled login or did not fully authorize.");
-          }
-        });
-       }
-       catch {        
-       }
-    }
+  login = async () => {
+     let FB:any = window;
+     try {
+      await FB.FB.login((response:any) => {
+        if (response.authResponse) {            
+          this.accesToken = response.authResponse.accessToken;            
+        } else {
+          console.log("User cancelled login or did not fully authorize.");
+        }
+      });
+     }
+     catch {        
+     }
+  }
 
-    getUsername = async () => {
-      const response = await fetch(`https://graph.facebook.com/me?fields=id,name&access_token=${this.accesToken}`);
+  getUsername = async () => {
+    const response = await fetch(`https://graph.facebook.com/me?fields=id,name&access_token=${this.accesToken}`);
+    const data = await response.json();
+    const userName = await data.name;
+    return userName;
+  }
+  getPitureUrl = async () => {
+      const response = await fetch(`https://graph.facebook.com/me?fields=id,name,picture&access_token=${this.accesToken}`);
       const data = await response.json();
       const userName = await data.name;
       return userName;
-    }
-    getPitureUrl = async () => {
-        const response = await fetch(`https://graph.facebook.com/me?fields=id,name,picture&access_token=${this.accesToken}`);
-        const data = await response.json();
-        const userName = await data.name;
-        return userName;
-    }
-    logout = async () => {
-        let FB:any = window;
-        FB.FB.logout((response:any) => {
-            console.log("user is now logged out");
-            
-          });
-    }
+  }
+  logout = async () => {
+      let FB:any = window;
+      FB.FB.logout((response:any) => {
+          console.log("user is now logged out");
+          
+        });
+  }
 
 
 }
